470f66dc07ee14971d3e45c6d529e0277720df7d
FoliniC <carlo.folini@gmail.com>
2026-01-04 14:24:16 +0100

    Fixed internationalizzation and switch graph (on/off)

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,6 @@ pytest_plugins = ["pytest_homeassistant_custom_component"]
 
 @pytest.fixture(scope="session")
 def event_loop():
-def event_loop():
     """Create an instance of the default event loop for each test case."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
